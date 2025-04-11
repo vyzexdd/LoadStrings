c8979f316da90993e4f2b2e4a7173d49e86c9eba
xᴢɪᴇ <ludvigaltidk13131319@gmail.com>
2025-04-11 16:02:31 +0200

    Update XzHub.lua

--- XzHub.lua
+++ XzHub.lua
@@ -102,16 +102,16 @@ Callback = function()
 local url = "https://raw.githubusercontent.com/Fspl0it/Blade-Ball/refs/heads/main/Game.lua"
 
 local success, response = pcall(function()
-    return game:HttpGet(url)
+return game:HttpGet(url)
 end)
 
 if success and response and not response:find("404: Not Found") then
-    local func, err = loadstring(response)
-    if func then
-        pcall(func)
-    else
-        warn("Failed to execute script:", err)
-    end
+local func, err = loadstring(response)
+if func then
+pcall(func)
+else
+warn("Failed to execute script:", err)
+end
 else
     warn("Not Loaded")
 end
